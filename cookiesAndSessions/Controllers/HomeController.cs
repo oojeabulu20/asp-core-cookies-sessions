22abeac9977a20a2805c8020ff150c97db5e887d
mustbefun <martinmustbebuilt@gmail.com>
2020-12-28 11:00:52 +0000

    New

---
+++ cookiesAndSessions/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using cookiesAndSessions.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace cookiesAndSessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (Request.Cookies["Colour"] == null)
+            {
+                ViewData["myColour"] = "red";
+            }
+            else
+            {
+                ViewData["myColour"] = Request.Cookies["Colour"].ToString();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection form)
+        {
+            string newColour = form["pickedColour"].ToString();
+            CookieOptions option = new CookieOptions();
+            option.Expires = DateTime.Now.AddMinutes(10);
+            Response.Cookies.Append("Colour", newColour, option);
+            ViewData["myColour"] = newColour;
+            return View();
+        }
+
+        public IActionResult TestCookie()
+        {
+            if (Request.Cookies["Colour"] == null)
+            {
+                ViewData["myColour"] = "red";
+            }
+            else
+            {
+                ViewData["myColour"] = Request.Cookies["Colour"].ToString();
+            }
+            return View();
+        }
+
+        public IActionResult DeleteCookies()
+        {
+            foreach (var cookie in HttpContext.Request.Cookies)
+            {
+                Response.Cookies.Delete("Colour");
+            }
+            return Redirect("/Home/Index");
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
